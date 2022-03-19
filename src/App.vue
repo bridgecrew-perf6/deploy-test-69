5cf9391d83016b26d06d726584f06a1a2d5a6c39
cmige <1437744965@qq.com>
2022-03-19 17:08:16 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+
+  <h2> init vue project</h2>
+
+  <h2>test deploy</h2>
+
+
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+}
+</script>
+
+<style>
+
+</style>
