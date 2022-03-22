8ae1a0ace3fbc44c92c127539cce40c4f85d06fe
cmige <1437744965@qq.com>
2022-03-22 13:23:19 +0800

    添加启动文件04

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 git pull
 yarn run build
 #删除docker容器
-docker rm -f nginx8080 &> /dev/null
-
-
+docker stop nginx8080
+docker rm -f nginx8080
 #启动容器
 docker run -d --restart=on-failure:10 -p 8080:80 -v $PWD/deploy-test/dist:/usr/share/nginx/html --name nginx8080 nginx
+echo $PWD
+echo '重新打包'
