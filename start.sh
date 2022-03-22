c4ae630236448acb84ca5508a8cd59602fbe9712
cmige <1437744965@qq.com>
2022-03-22 13:56:37 +0800

    添加启动文件05

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ yarn run build
 docker stop nginx8080
 docker rm -f nginx8080
 #启动容器
-docker run -d --restart=on-failure:10 -p 8080:80 -v $PWD/deploy-test/dist:/usr/share/nginx/html --name nginx8080 nginx
+docker run -d --restart=on-failure:10 -p 8080:80 -v $PWD/dist:/usr/share/nginx/html --name nginx8080 nginx
 echo $PWD
 echo '重新打包'
