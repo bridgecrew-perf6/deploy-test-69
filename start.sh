ca46b437e5f72e3452ddb4cba4ca4e8cad91c449
cmige <1437744965@qq.com>
2022-03-22 12:52:09 +0800

    添加启动文件02

--- start.sh
+++ start.sh
@@ -2,6 +2,9 @@ git pull
 
 #删除docker容器
 docker rm -f nginx8080 &> /dev/null
+echo '脚本运行'
 
 #启动容器
 docker run -d --restart=on-failure:10 -p 8080:80 -v $PWD/dist:/user/share/nginx/html --name nginx8080 nginx
+echo '启动容器'
+echo '$PWD'
