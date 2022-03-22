3231618b5a3d7066cb716f1fa64b8da408a6a0a7
cmige <1437744965@qq.com>
2022-03-22 13:20:30 +0800

    添加启动文件03

--- start.sh
+++ start.sh
@@ -2,9 +2,7 @@ git pull
 yarn run build
 #删除docker容器
 docker rm -f nginx8080 &> /dev/null
-echo '脚本运行'
+
 
 #启动容器
-docker run -d --restart=on-failure:10 -p 8080:80 -v /home/mutou/app/deploy-test/dist:/usr/share/nginx/html --name nginx8080 nginx
-echo '启动容器'
-echo $PWD
+docker run -d --restart=on-failure:10 -p 8080:80 -v $PWD/deploy-test/dist:/usr/share/nginx/html --name nginx8080 nginx
